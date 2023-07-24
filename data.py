3bb7a342d4bfa13e4d5c5bed52b6701e2d3edfc3
bobyyandfriends <joshuaaltman1@gmail.com>
2023-07-23 21:20:36 -0400

    Full fix

--- data.py
+++ data.py
@@ -5,17 +5,17 @@ import datetime
 # Define the stock symbol and date range
 symbol = "AAPL"  # Example stock symbol (Apple Inc.)
 start_date = "2022-01-01"  # Example start date
-end_date = "2022-12-31"  # Example end date
+end_date = "2022-12-31"  # Example end dateW
 
 
 today = datetime.datetime.now()
 end_date = today
 #print("Today is " + str(today.strftime("%b-%d-%Y")))
-print(end_date)
+
 fifteen_years_ago = today - datetime.timedelta(days=15*365)
 start_date = fifteen_years_ago
 #print("The day fifteen years ago was " + str(fifteen_years_ago.strftime("%b-%d-%Y")))
-print(start_date)
+
 
 # Fetch historical stock price data from Yahoo Finance
 data = yf.download(symbol, start=start_date, end=end_date)
